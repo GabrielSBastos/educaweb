ce70ab5d7ebcc07a2ee4e690681fa9d469e57791
Gabriel <gabrielsou1912@gmail.com>
2023-11-13 07:29:45 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,css,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      backgroundform:'#0D5DA6',
+    }
+  },
+  plugins: [],
+}
+
